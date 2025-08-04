7ef534b2d58a5abcfd61ce1ccef2da9ac9e0f38d
51hhh <zzhhh2005@gmail.com>
2025-08-04 14:56:36 +0800

    提交

--- api/device.py
+++ api/device.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify
 import json
+import os
 
 device_bp = Blueprint('device', __name__)
 
 @device_bp.route('/device_info')
 def get_device_info():
-    with open('./data/device_info.json', 'r', encoding='utf-8') as f:
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'device_info.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
         device_info = json.load(f)
     return jsonify(device_info)
